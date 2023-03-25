bb1bc865bc29d59ff6ebfc7d90d5fa77743a1162
Hamim Alam <82621331+alamhamim@users.noreply.github.com>
2023-03-24 23:44:19 -0400

    install file

--- install.sh
+++ install.sh
@@ -14,10 +14,12 @@ rm -r .git
 rm *.sh
 rm *.md
 
-rm -r __MACOSX
+
 
 # Unzip the java_maven.zip file
 unzip java_maven.zip
+
+rm -r __MACOSX
 
 # Remove the java_maven.zip file
 rm java_maven.zip
@@ -25,8 +27,10 @@ rm java_maven.zip
 #Adding them in the system variable
 #!/bin/bash
 
+#!/bin/bash
+
 # Check if the user is using zsh
-elif [[ "$SHELL" == "/bin/zsh" ]]; then
+if [[ "$SHELL" == "/bin/zsh" ]]; then
     # If the user is using zsh, add the environment variables to .zshrc
     echo 'export JAVA_HOME=~/sSoftware/all_in_one/java_maven/jdk-11.0.18.jdk/Contents/Home' >> ~/.zshrc
     echo 'export PATH=$JAVA_HOME/bin:$PATH' >> ~/.zshrc
@@ -36,19 +40,18 @@ elif [[ "$SHELL" == "/bin/zsh" ]]; then
 elif [[ "$SHELL" == "/bin/bash" ]]; then
     # If the user is using bash, add the environment variables to .bashrc or .bash_profile
     if [ -f "$HOME/.bashrc" ]; then
-
         echo 'export JAVA_HOME=~/sSoftware/all_in_one/java_maven/jdk-11.0.18.jdk/Contents/Home' >> ~/.bashrc
         echo 'export PATH=$JAVA_HOME/bin:$PATH' >> ~/.bashrc
         echo 'export MAVEN_HOME=~/sSoftware/all_in_one/java_maven/apache-maven-3.9.0' >> ~/.bashrc
         echo 'export PATH=$MAVEN_HOME/bin:$PATH' >> ~/.bashrc
     elif [ -f "$HOME/.bash_profile" ]; then
-       
         echo 'export JAVA_HOME=~/sSoftware/all_in_one/java_maven/jdk-11.0.18.jdk/Contents/Home' >> ~/.bash_profile
         echo 'export PATH=$JAVA_HOME/bin:$PATH' >> ~/.bash_profile
         echo 'export MAVEN_HOME=~/sSoftware/all_in_one/java_maven/apache-maven-3.9.0' >> ~/.bash_profile
         echo 'export PATH=$MAVEN_HOME/bin:$PATH' >> ~/.bash_profile
     fi
 fi
+
 
 echo "THANK YOU!!! For installing :)"
 #THE END!
