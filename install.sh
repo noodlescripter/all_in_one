417fcd8e8a4ea8626692c0adfcdca91b2c7145ca
Hamim Alam <82621331+alamhamim@users.noreply.github.com>
2023-03-24 23:52:33 -0400

    many more changes

--- install.sh
+++ install.sh
@@ -32,22 +32,22 @@ rm -r __MACOSX
 # Check if the user is using zsh
 if [[ "$SHELL" == "/bin/zsh" ]]; then
     # If the user is using zsh, add the environment variables to .zshrc
-    echo 'export JAVA_HOME=~/sSoftware/all_in_one/java_maven/jdk-11.0.18.jdk/Contents/Home' >> ~/.zshrc
+    echo 'export JAVA_HOME=~/sSoftware/all_in_one/jdk-11.0.18.jdk/Contents/Home' >> ~/.zshrc
     echo 'export PATH=$JAVA_HOME/bin:$PATH' >> ~/.zshrc
-    echo 'export MAVEN_HOME=~/sSoftware/all_in_one/java_maven/apache-maven-3.9.0' >> ~/.zshrc
+    echo 'export MAVEN_HOME=~/sSoftware/all_in_one/apache-maven-3.9.0' >> ~/.zshrc
     echo 'export PATH=$MAVEN_HOME/bin:$PATH' >> ~/.zshrc
 # Check if the user is using bash
 elif [[ "$SHELL" == "/bin/bash" ]]; then
     # If the user is using bash, add the environment variables to .bashrc or .bash_profile
     if [ -f "$HOME/.bashrc" ]; then
-        echo 'export JAVA_HOME=~/sSoftware/all_in_one/java_maven/jdk-11.0.18.jdk/Contents/Home' >> ~/.bashrc
+        echo 'export JAVA_HOME=~/sSoftware/all_in_one/jdk-11.0.18.jdk/Contents/Home' >> ~/.bashrc
         echo 'export PATH=$JAVA_HOME/bin:$PATH' >> ~/.bashrc
-        echo 'export MAVEN_HOME=~/sSoftware/all_in_one/java_maven/apache-maven-3.9.0' >> ~/.bashrc
+        echo 'export MAVEN_HOME=~/sSoftware/all_in_one/apache-maven-3.9.0' >> ~/.bashrc
         echo 'export PATH=$MAVEN_HOME/bin:$PATH' >> ~/.bashrc
     elif [ -f "$HOME/.bash_profile" ]; then
-        echo 'export JAVA_HOME=~/sSoftware/all_in_one/java_maven/jdk-11.0.18.jdk/Contents/Home' >> ~/.bash_profile
+        echo 'export JAVA_HOME=~/sSoftware/all_in_one/jdk-11.0.18.jdk/Contents/Home' >> ~/.bash_profile
         echo 'export PATH=$JAVA_HOME/bin:$PATH' >> ~/.bash_profile
-        echo 'export MAVEN_HOME=~/sSoftware/all_in_one/java_maven/apache-maven-3.9.0' >> ~/.bash_profile
+        echo 'export MAVEN_HOME=~/sSoftware/all_in_one/apache-maven-3.9.0' >> ~/.bash_profile
         echo 'export PATH=$MAVEN_HOME/bin:$PATH' >> ~/.bash_profile
     fi
 fi
